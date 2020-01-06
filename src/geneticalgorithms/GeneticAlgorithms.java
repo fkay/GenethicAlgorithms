7a9278d9b7b108fb8501157fcec770a20253d771
Fabricio Kassardjian <fkay13@gmail.com>
2020-01-06 08:51:04 -0300

    Testes finais

--- src/geneticalgorithms/GeneticAlgorithms.java
+++ src/geneticalgorithms/GeneticAlgorithms.java
@@ -11,9 +11,7 @@ import geneticalgorithms.cromossomes.*;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Locale;
 
 /**
